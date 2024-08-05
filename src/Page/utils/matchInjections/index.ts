17c4fed3411c83b8277312585f813d87c2bc8fb4
vvapnik <vladislavvapnik@gmail.com>
2024-08-05 09:33:39 +0200

    refactoring of matching

---
+++ src/Page/utils/matchInjections/index.ts
@@ -0,0 +1,7 @@
+import {InjectableMetadata, ServiceState} from "../../types";
+import {ServicePool} from "../../ServicePool";
+
+export function matchInjections({params}: InjectableMetadata, servicePool: ServicePool): ServiceState[] {
+    if (!params || !params.length) return []
+    return params.map(dep => servicePool.getServiceByClass(dep))
+}
