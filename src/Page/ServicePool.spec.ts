f0d2c08b3d011ed242162dd3070e5e903e713dd9
vvapnik <vladislavvapnik@gmail.com>
2024-08-11 10:21:32 +0200

    Service Pool and wait helper

--- src/Page/ServicePool.spec.ts
+++ src/Page/ServicePool.spec.ts
@@ -1,3 +1,123 @@
-describe('Page/ServicePool', () => {
+import {Service} from "../Service";
+import {ServicePool} from "./ServicePool";
+import {EmptyService} from "../Service/EmptyService";
+import {wait} from "../utils/wait";
 
+describe('Page/ServicePool', () => {
+    it('should find a service by class', async () => {
+        const handler = jest.fn()
+
+        @Service()
+        class SimpleService {
+            async exec() {
+                handler()
+            }
+        }
+
+        const services = new ServicePool([SimpleService])
+
+        const simpleInstance = services.getServiceByClass(SimpleService)
+        await simpleInstance.instance.exec()
+        expect(handler.mock.calls.length).toBe(1)
+        expect(simpleInstance.mainClass === SimpleService).toBeTruthy()
+    })
+    it('should initiate services in correct order', () => {
+        const results = []
+
+        @Service()
+        class Service1 extends EmptyService {
+            constructor() {
+                super()
+                results.push('1')
+            }
+        }
+
+        @Service()
+        class Service2 extends EmptyService {
+            constructor(_: Service1) {
+                super();
+                results.push('2')
+            }
+
+        }
+
+        @Service()
+        class Service3 extends EmptyService {
+            constructor(_: Service1, __: Service2) {
+                super();
+                results.push('3')
+            }
+        }
+
+        new ServicePool([Service1, Service2, Service3])
+        expect(results.join('')).toBe('123')
+    })
+
+    it('should throw an error if services initialise in wrong order', () => {
+        const results = []
+
+        @Service()
+        class Service1 extends EmptyService {
+            constructor() {
+                super()
+                results.push('1')
+            }
+        }
+
+        @Service()
+        class Service2 extends EmptyService {
+            constructor(_: Service1) {
+                super();
+                results.push('2')
+            }
+
+        }
+
+        @Service()
+        class Service3 extends EmptyService {
+            constructor(_: Service1, __: Service2) {
+                super();
+                results.push('3')
+            }
+        }
+
+        expect(() => new ServicePool([Service2, Service3, Service1])).toThrow()
+    })
+    it('should execute services in correct order', async () => {
+        const results = []
+
+        @Service()
+        class Service1 {
+            async exec() {
+                await wait(300)
+                results.push('1')
+            }
+        }
+
+        @Service()
+        class Service2 {
+            constructor(_: Service1) {
+            }
+
+            async exec() {
+                await wait(200)
+                results.push('2')
+            }
+        }
+
+        @Service()
+        class Service3 {
+            constructor(_: Service1, __: Service2) {
+            }
+
+            async exec() {
+                await wait(100)
+                results.push('3')
+            }
+        }
+
+        const services = new ServicePool([Service1, Service2, Service3])
+        await services.executeServices()
+        expect(results.join('')).toBe('123')
+    })
 })
--- src/Page/ServicePool.ts
+++ src/Page/ServicePool.ts
@@ -1,20 +1,19 @@
 import {PreService} from "../Service/types";
 import {PoolStatus} from "./ServicePool.c";
 
-//TODO: check config
-// @ts-ignore
-import {InjectableMetadata, ServiceState} from "./types.d.ts";
+import {ServiceState} from "./types";
 import {getInjectableMetadata} from "./utils/getInjectableMetadata";
 import {validateService} from "./utils/metaValidation";
 import {matchInjections} from "./utils/matchInjections";
+import {executeService} from "./utils/executeService";
 
 export class ServicePool {
     public status = PoolStatus.initializing
-    protected services: ServiceState[]
+    protected services: ServiceState[] = []
 
     constructor(services: PreService[]) {
         services.forEach(service => this.initService(service))
-
+        this.status = PoolStatus.pending
     }
 
     initService(service: PreService) {
@@ -25,7 +24,7 @@ export class ServicePool {
             ...metadata,
             mainClass: service,
             executed: false,
-            instance: service(),
+            instance: new service(),
             dependencies
         })
     }
@@ -36,4 +35,15 @@ export class ServicePool {
         if (!service) throw Error(`service ${target} is injected before initialization`)
         return service
     }
+
+    public async executeServices(): Promise<void> {
+        this.status = PoolStatus.executing
+        const promises = []
+        for (let service of this.services) {
+            promises.push(executeService(service))
+        }
+        await Promise.all(promises)
+        this.status = PoolStatus.ready
+    }
+
 }
